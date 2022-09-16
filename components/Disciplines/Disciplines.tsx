fb161d61cb67faa0f1ef313f76f2c1d3dc9325cf
Evgenie <nestroykozni@gmail.com>
2022-09-16 06:01:24 +0300

    feat: style Disciplines component

--- components/Disciplines/Disciplines.tsx
+++ components/Disciplines/Disciplines.tsx
@@ -6,33 +6,31 @@ interface Props {
 	data: Data[];
 }
 
-const Disciplines: React.FC<Props> = ({ data }) => {
+export const Disciplines: React.FC<Props> = ({ data }) => {
 	return (
 		<>
-			{data.map(({ title, specializedSubjects, baseSubjects, id }) => {
-				const module1 = baseSubjects.slice(0, 5);
-				const module2 = baseSubjects.slice(5, 10);
+			{data.map(({ title, specializedSubjects, id }) => {
 				return (
-					<div key={id}>
+					<div key={id} className={styles.programWrap}>
 						<h2 className={styles.title}>{title}</h2>
-						<div className={styles.moduleWrap}>
-							<div>
+						<div className={styles.modules}>
+							<div className={styles.stickWrap}>
 								<div className={styles.stick} />
 								<h3 className={styles.moduleName}>1 модуль</h3>
 							</div>
 							<ul className={styles.disciplinesWrap}>
-								{module1.map(({ string, id }) => (
+								{specializedSubjects.map(({ string, id }) => (
 									<li key={id} className={styles.disciplines}>
 										{string}
 									</li>
 								))}
 							</ul>
-							<div>
+							<div className={styles.stickWrap}>
 								<div className={styles.stick} />
 								<h3 className={styles.moduleName}>2 модуль</h3>
 							</div>
 							<ul className={styles.disciplinesWrap}>
-								{module2.map(({ string, id }) => (
+								{specializedSubjects.map(({ string, id }) => (
 									<li key={id} className={styles.disciplines}>
 										{string}
 									</li>
